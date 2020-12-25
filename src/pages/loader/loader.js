dc67927775072151e122809e2e28284e35a2693f
Thang Tran <tran.tkhanh@outlook.com>
2020-12-25 15:08:24 +0200

    Edit account page. Added loader component.

---
+++ src/pages/loader/loader.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./loader.css";
+
+export const Loader = () => {
+    return (
+        <div className="loader-text">
+            <div className="loader-header">
+                <div className="loading loading04">
+                    <span>Ъ</span>
+                    <span>Ъ</span>
+                    <span>Ъ</span>
+                    <span>Ъ</span>
+                    <span>Ъ</span>
+                    <span>Ъ</span>
+                    <span>Ъ</span>
+                </div>
+            </div>
+        </div>
+    )
+}
